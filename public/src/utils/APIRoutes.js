a4ad029022ef2081b8418213dcf11fe43f4dba8f
Tushar Pamnani <121151091+tusharpamnani@users.noreply.github.com>
2023-08-17 15:33:40 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://chat-app--server.vercel.app/";
+export const host = "https://chat-app--server.vercel.app";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
